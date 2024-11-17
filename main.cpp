2ab01272b118df2e7e43839947c6ce0c18a83bc1
Dmytro Varchuk <loltop792@gmail.com>
2024-11-17 15:02:57 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int corner_top_right = 187;
+    int corner_top_left = 201;
+    int corner_bottom_right = 188;
+    int corner_bottom_left = 200;
+    int horizontal_line = 205;
+    int vertical_line = 186;
+	int separator_left = 204;	
+	int separator_right = 185;
+
+
+	cout << char(corner_top_left)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+		<< char(corner_top_right);
+	cout << endl;
+	cout << char(vertical_line)
+		<< "                      Vremena Goda                 "  << char(vertical_line) << endl;	
+	    cout << char(separator_left)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(separator_right);
+	cout << endl;
+	    cout<< char(vertical_line)
+		<< "    Zima    " << char(vertical_line) << "   Vesna    "
+		<< char(vertical_line) << "    Leto    " << char(vertical_line)
+		<< "    Osen    " << char(vertical_line) << endl;
+		cout << char(corner_bottom_left)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(horizontal_line) << char(horizontal_line) << char(horizontal_line)
+			<< char(corner_bottom_right);
+
+    return 0;
+}
