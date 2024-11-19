279329624d2fa14cd7a2dece5e09cd54140ed206
Dmytro Varchuk <loltop792@gmail.com>
2024-11-19 15:59:12 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream> 
+
+using namespace std; 
+
+int main()
+{
+	system("color 0E"); 
+	cout << "U Lukomor'a dub zelenij, " << endl;
+	cout << "Zlataya zep na dupe tom, " << endl;
+	cout << "I dnem i nochju kot uchenij " << endl;
+	cout << "vse hodit po cepi krugom" << endl;
+
+	return 0; 
+}
